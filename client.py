ec8745c1aea4617a1f9c70ae2228eb4e4395f82c
lakshmiNageswari <lakshmi.nr@iitgn.ac.in>
2022-09-16 20:58:09 +0530

    first

---
+++ client.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import socket
+import os
+import tqdm
+HOST =  socket.gethostname()  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+cmd=str(input())
+command = cmd.split()
+
+
+def decrypt(parameter,mode):
+    
+    mode=int(mode)
+    lst=parameter.split()
+    # print(lst)
+    final=[]
+    for j in range(len(lst)):
+        data=lst[j]
+        if mode==0: 
+            final.append(data.decode())
+        
+        elif mode==1:
+            s=""
+            offset=2
+            for r in data:
+             
+                p=chr(r)
+             
+                if p.isalpha():
+                    s=s+chr(ord(p)-offset)
+                    
+                elif p.isdigit():
+                    s=s+str(int(p)-offset)
+                    
+                else:
+                    s=s+p
+                    
+            final.append(s)
+            
+        else:
+            
+            final.append(data[::-1].decode())
+            
+            
+    return " ".join(final)
+
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.send(cmd.encode())
+    
+    k=s.recv(1024).decode()
+    while k:
+       
+    
+        if command[0]=="UPD":
+            s.send("testing".encode())
+            filename=command[1]
+            filename = os.path.basename(filename)
+            print(filename)
+            
+            f = open(filename,'rb')
+            l = f.read(1024)
+            while (l):
+                s.send(l)
+                print('Sent ',repr(l))
+                l = f.read(1024)
+            f.close()
+            s.close()
+            
+            # print("file closed!")
+            break 
+        
+        elif command[0]=="DWD":
+            
+            current=os.getcwd()
+                
+            with open('download_file', 'wb') as f:
+                 
+                    while True:
+                        # print('receiving data...')
+                        content = s.recv(1024)
+                        # print(decrypt(content,command[2]))
+                        # print(content)
+                        
+                        if not content:
+                            break
+                      
+                        f.write(decrypt(content,command[2]).encode())
+                    f.close()
+                    
+            print("file closed!")
+            s.close()
+            break
+                    
+       
+            
+        else:
+            data = s.recv(1024).decode()
+            print(data)
+            s.close()
+            break
+        
+    
---
+++ server.py
@@ -0,0 +1,112 @@
+
+
+import socket
+import os
+import tqdm
+HOST = socket.gethostname()  
+PORT = 65432 
+
+def encrypt(parameter,mode):
+    
+    mode=int(mode)
+    lst=parameter.split()
+ 
+    final=[]
+    for j in range(len(lst)):
+        data=lst[j]
+        if mode==0: 
+            final.append(data.decode())
+        
+        elif mode==1:
+            s=""
+            offset=2
+            for r in data:
+             
+                p=chr(r)
+             
+                if p.isalpha():
+                    s=s+chr(ord(p)+offset)
+                    
+                elif p.isdigit():
+                    s=s+str(int(p)+offset)
+                    
+                else:
+                    s=s+p
+                    
+            final.append(s)
+            
+        else:
+            
+            final.append(data[::-1].decode())
+            
+            
+    return " ".join(final)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        # while True:
+            print(f"Connected by {addr}")
+            data = conn.recv(1024).decode()
+            data = data.split(' ')
+         
+            conn.send("Arguments are received!".encode())
+          
+            if data[0]=="CWD":
+                conn.send(os.getcwd().encode())
+            elif data[0]=="LS":
+                files = [f for f in os.listdir('.') if os.path.isfile(f)]
+                files=str(files)
+                conn.send(files.encode())
+            elif data[0]=="CD":
+                os.chdir(data[1])
+                conn.send(os.getcwd().encode())
+                
+            elif data[0]=="UPD":
+      
+                receiving=conn.recv(1024).decode()
+               
+                with open('uploaded_file', 'wb') as f:
+                    
+                    while True:
+                        print('receiving data...')
+                        content = conn.recv(1024)
+                     
+                        if not content:
+                            break
+                    
+                        print(content)
+                        f.write(content)
+                    f.close()
+          
+                        
+            elif data[0]=="DWD":
+                filename=data[1]
+               
+                
+                f = open(filename,'rb')
+                l = f.read(1024)
+                while (l):
+                    print(str(l))
+                    encrypted = encrypt(l,data[2]).encode()
+                 
+                    conn.send(encrypted)
+                   
+                    l = f.read(1024)
+                f.close()
+                        
+            else:
+                print("no proper input")
+                
+            conn.close()
+                
+                
+               
+                
+
+                
+  
+                
